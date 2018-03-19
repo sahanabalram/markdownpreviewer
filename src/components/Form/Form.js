22f8974a347321719a91b47c5b39e9f8f32e2c82
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 15:23:12 -0700

    Update input dynamically

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,13 +3,28 @@ import {FormGroup, ControlLabel, FormControl} from "react-bootstrap";
 
 let marked = require("markdown");
 class Form extends Component {
+    state = {
+        markdown: ""
+    }
+// to update value dynamically use built in javascript method onChange
+    updateMarkdown = function(markdown) {
+        this.setState({markdown: markdown});
+    }
     render() {
+        let {markdown} = this.state;
+        console.log(markdown);
         return (
             <div className="container">
-                <FormGroup controlId="formControlsTextarea">
-                    <ControlLabel>Markdown Area</ControlLabel>
-                    <FormControl componentClass="textarea" placeholder="Enter Markdown"/>
-                </FormGroup>
+                <div>
+                    <FormGroup controlId="formControlsTextarea">
+                        <ControlLabel>Markdown Area</ControlLabel>
+                        <FormControl componentClass="textarea" placeholder="Enter Markdown" value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}/>
+                    </FormGroup>
+                </div>
+                <div>
+                    <h2>Markdown Output</h2>
+                    <div></div>
+                </div>
             </div>
         )
     }
