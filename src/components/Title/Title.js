929ee3cae5acf235f964898ca4fdc3c9fc0c9ce8
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 14:48:35 -0700

    Title Component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class Title extends Component {
+    render(){
+        return(
+            <div>Title Component</div>
+        )
+    }
+}
+
+export default Title;
