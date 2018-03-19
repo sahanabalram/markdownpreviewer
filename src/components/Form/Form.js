47bd1c4370fb38d74072421c53c576e767037cd9
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 15:03:02 -0700

    Form Component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import {FormGroup,ControlLabel,FormControl} from "react-bootstrap";
+class Form extends Component {
+    render(){
+        return(
+            <div>Form Component</div>
+        )
+    }
+}
+
+export default Form;
